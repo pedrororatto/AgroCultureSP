8bea573c5b026d2e90717d0e2fea9281c61ee3dc
Pedro Roratto <pedro_roratto@hotmail.com>
2023-11-26 11:48:02 -0300

    Add python environment

--- src/classes/culture.py
+++ src/classes/culture.py
@@ -2,27 +2,9 @@ import pandas as pd
 import os
 
 class Culture:
-    def __init__(self):
-        self.data = None
-
-    def import_csv(self):
-        file_path = os.path.join(os.path.dirname(__file__), '..', 'database', 'meu_arquivo.csv')
-        self.data = pd.read_csv(file_path)
-
-    def display_information(self):
-        if self.data is None:
-            print("CSV file not imported. Please call import_csv() method first.")
-        else:
-            for index, row in self.data.iterrows():
-                city = row['CITY']
-                period = row['PERIOD']
-                max_climate = row['MAX_CLIMATE']
-                min_climate = row['MIN_CLIMATE']
-                soil = row['SOIL']
-                culture = row['CULTURE']
-                print(f"City: {city}, Period: {period}, Max Climate: {max_climate}, Min Climate: {min_climate}, Soil: {soil}, Culture: {culture}")
-
-# Usage example
-culture = Culture()
-culture.import_csv()
-culture.display_information()
+    def __init__(self, name, path):
+        self.name = name
+        self.path = path
+        self.data = pd.read_csv(path, sep='\t', index_col=0)
+        self.data = self.data.fillna(0)
+        self.data = self.data.astype(int)
